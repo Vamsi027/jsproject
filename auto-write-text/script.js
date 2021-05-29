bd9a94a93cac7b3d2d3005a906efd7e8ad5c481b
Vamsi027 <you@example.com>
2021-05-29 10:36:34 +0530

    auto write text

---
+++ auto-write-text/script.js
@@ -0,0 +1,15 @@
+const text = "Just testing how to auto write text in js";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
